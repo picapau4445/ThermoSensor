1438df86a5be078c2ac6d364d8f153ba9d36917a
大橋 真吾 <ohashi.s@nds-tyo.co.jp>
2017-07-17 07:51:07 +0900

    PayloadのdeviceIdを文字列として送るよう修正

--- thermoSensorPayload.py
+++ thermoSensorPayload.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 def getPayloadString(device_id, temperature):
     now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    payload = '{"temperature": ' + temperature + ', "recDate": "' + now + '", "deviceId": ' + device_id + '}'
+    payload = '{"temperature": ' + temperature + ', "recDate": "' + now + '", "deviceId": "' + device_id + '"}'
     return payload
